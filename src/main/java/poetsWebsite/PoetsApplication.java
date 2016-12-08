18bb0cdee460996aa7adb737c36d702c18ae5b9f
tanukaaa <tanuka@mail.bg>
2016-12-08 14:00:51 +0200

    Uploading Files to github

---
+++ src/main/java/poetsWebsite/PoetsApplication.java
@@ -0,0 +1,12 @@
+package poetsWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetsApplication.class, args);
+	}
+}
---
+++ src/test/java/poetsWebsite/PoetsApplicationTests.java
@@ -0,0 +1,16 @@
+package poetsWebsite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PoetsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
