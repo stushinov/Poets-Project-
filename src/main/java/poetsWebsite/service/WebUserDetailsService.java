92540e08f77e69dd0a410a9febb3ab2159a6cb03
tanukaaa <tanuka@mail.bg>
2016-12-08 14:57:35 +0200

    Created User Service

---
+++ src/main/java/poetsWebsite/service/WebUserDetailsService.java
@@ -0,0 +1,49 @@
+package poetsWebsite.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import poetsWebsite.entity.User;
+import poetsWebsite.repository.UserRepository;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Admin on 8.12.2016 г..
+ */
+@Service("webUserDetailsService")
+public class WebUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public WebUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Invalid user");
+        } else {
+
+            Set<GrantedAuthority> grantedAuthorities = user.getRoles()
+                    .stream()
+                    .map(role -> new SimpleGrantedAuthority(role.getName()))
+                    .collect(Collectors.toSet());
+
+            return new org
+                    .springframework
+                    .security
+                    .core
+                    .userdetails
+                    .User(user.getEmail(), user.getPassword(), grantedAuthorities);
+        }
+    }
+}
