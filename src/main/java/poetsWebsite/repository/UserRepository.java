34314a9477041c601e7b3dee7b262bc186eeafb0
tanukaaa <tanuka@mail.bg>
2016-12-08 14:41:37 +0200

    Created User Repo

---
+++ src/main/java/poetsWebsite/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package poetsWebsite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import poetsWebsite.entity.User;
+
+/**
+ * Created by Admin on 8.12.2016 г..
+ */
+public interface UserRepository  extends JpaRepository<User, Integer> {
+
+    //Black magic
+    User findByEmail(String email);
+}
