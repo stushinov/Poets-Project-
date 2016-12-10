6ce0304f2a807b82e0b9fabc53e39f45e18f5077
tanukaaa <tanuka@mail.bg>
2016-12-10 13:43:39 +0200

    Added UserBindingModel

---
+++ src/main/java/poetsWebsite/bindingModel/UserBindingModel.java
@@ -0,0 +1,68 @@
+package poetsWebsite.bindingModel;
+
+import com.sun.istack.internal.Nullable;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Admin on 10.12.2016 г..
+ */
+public class UserBindingModel {
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String fullName;
+
+    @Nullable
+    private String city;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String confirmPassword;
+
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
