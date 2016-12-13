873dfcdf522bc079c8e710312a535b81de7916e7
tanukaaa <tanuka@mail.bg>
2016-12-13 21:42:04 +0200

    Live chat now stores the submited values correctly in the database

--- src/main/java/poetsWebsite/controller/ChatController.java
+++ src/main/java/poetsWebsite/controller/ChatController.java
@@ -1,11 +1,17 @@
 package poetsWebsite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import poetsWebsite.bindingModel.ChatBindingModel;
+import poetsWebsite.entity.Chat;
 import poetsWebsite.entity.User;
+import poetsWebsite.repository.ChatRepository;
 import poetsWebsite.repository.UserRepository;
 
 /**
@@ -13,6 +19,12 @@ import poetsWebsite.repository.UserRepository;
  */
 @Controller
 public class ChatController {
+
+    @Autowired
+    ChatRepository chatRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/chat")
     public String chat(Model model){
@@ -22,5 +34,30 @@ public class ChatController {
         return "layout";
     }
 
+    @PostMapping("/chat")
+    public String chatProcess(ChatBindingModel chatBindingModel){
 
+        String author = "";
+
+        try {
+            UserDetails user = (UserDetails) SecurityContextHolder.getContext()
+                    .getAuthentication()
+                    .getPrincipal();
+
+            User getUserDetails = userRepository.findByEmail(user.getUsername());
+            author = getUserDetails.getFullName();
+
+        } catch (Exception e){
+            author = "Anonymous";
+        }
+            chatBindingModel.setAuthor(author);
+        Chat chatEntity = new Chat(
+                chatBindingModel.getAuthor(),
+                chatBindingModel.getContent()
+        );
+
+        this.chatRepository.saveAndFlush(chatEntity);
+
+        return "redirect:/chat";
+    }
 }
