fba5b449e36d6bda4b3af42ae7fcf2710ad03ca4
tanukaaa <tanuka@mail.bg>
2016-12-14 15:19:33 +0200

    Created Article Binding Model

---
+++ src/main/java/poetsWebsite/bindingModel/ArticleBindingModel.java
@@ -0,0 +1,34 @@
+package poetsWebsite.bindingModel;
+
+import poetsWebsite.entity.User;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Admin on 14.12.2016 г..
+ */
+public class ArticleBindingModel {
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+    
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
