ace0ac7b4dcaae403f584ca2257ad9b7a667009f
tanukaaa <tanuka@mail.bg>
2016-12-18 18:57:01 +0200

    Implemented the read more button

--- src/main/java/poetsWebsite/controller/ArticleController.java
+++ src/main/java/poetsWebsite/controller/ArticleController.java
@@ -1,11 +1,13 @@
 package poetsWebsite.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import poetsWebsite.bindingModel.ArticleBindingModel;
 import poetsWebsite.entity.Article;
@@ -79,5 +81,31 @@ public class ArticleController {
         this.articleRepository.saveAndFlush(articleEntity);
 
         return "redirect:/articles";
+    }
+
+
+    @GetMapping("/article/{id}")
+    public String details(Model model, @PathVariable Integer id){
+        if(!this.articleRepository.exists(id)){
+            return "redirect:/";
+        }
+
+
+        //This will get our authentication token and check if the type of user is anonymous.
+        //If the authentication is anonymous it means that we don't have logged in user.
+        //In the body of our if, we need to get our current use
+        if(!(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)){
+            UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            User entityUser = this.userRepository.findByEmail(principal.getUsername());
+
+            model.addAttribute("user", entityUser);
+        }
+
+        Article article = this.articleRepository.findOne(id);
+
+        model.addAttribute("article", article);
+        model.addAttribute("view", "articles/details");
+
+        return "layout";
     }
 }
