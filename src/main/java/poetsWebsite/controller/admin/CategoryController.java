1438171fa11dc8d48455abb50274b81acf3612d7
tanukaaa <tanuka@mail.bg>
2016-12-21 01:45:13 +0200

    Implemented the category edit button in the admins panel

--- src/main/java/poetsWebsite/controller/admin/CategoryController.java
+++ src/main/java/poetsWebsite/controller/admin/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import poetsWebsite.bindingModel.CategoryBindingModel;
@@ -29,6 +30,8 @@ public class CategoryController {
     @Autowired
     private ArticleRepository articleRepository;
 
+
+    // /LIST
     @GetMapping("/")
     public String list(Model model){
 
@@ -43,12 +46,13 @@ public class CategoryController {
         return "layout";
     }
 
+
+    //CREATE
     @GetMapping("/create")
     public String create(Model model){
         model.addAttribute("view", "admin/category/create");
         return "layout";
     }
-
     @PostMapping("/create")
     public String createProcess(CategoryBindingModel categoryBindingModel){
 
@@ -63,4 +67,39 @@ public class CategoryController {
 
         return "redirect:/admin/categories/";
     }
+
+
+    //EDIT
+    @GetMapping("/edit/{id}")
+    public String edit(Model model, @PathVariable Integer id){
+
+        if(!this.categoryRepository.exists(id)){
+            return "redirect:/admin/categories/";
+        }
+
+        //Gets current category
+        Category category = this.categoryRepository.findOne(id);
+
+        model.addAttribute("category", category);
+        model.addAttribute("view", "admin/category/edit");
+
+        return "layout";
+    }
+    @PostMapping("/edit/{id}")
+    public String createProcess(@PathVariable Integer id, CategoryBindingModel categoryBindingModel){
+
+        if(!this.categoryRepository.exists(id)){
+            return "redirect:/admin/categories/";
+        }
+
+        Category category = this.categoryRepository.findOne(id);
+
+        category.setName(categoryBindingModel.getName());
+
+        this.categoryRepository.saveAndFlush(category);
+
+        return "redirect:/admin/categories/";
+    }
+
+
 }
