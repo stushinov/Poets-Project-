3ee1f88e03ecfb09628c2c9f6f5c0c78d85b8f57
tanukaaa <tanuka@mail.bg>
2016-12-20 21:32:44 +0200

    Implemented user delete button in the admin pannel

--- src/main/java/poetsWebsite/controller/admin/AdminUserController.java
+++ src/main/java/poetsWebsite/controller/admin/AdminUserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import poetsWebsite.bindingModel.UserEditBindingModel;
+import poetsWebsite.entity.Article;
 import poetsWebsite.entity.Role;
 import poetsWebsite.entity.User;
 import poetsWebsite.repository.ArticleRepository;
@@ -107,6 +108,43 @@ public class AdminUserController {
 
         return "redirect:/admin/users/";
 
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, Model model){
+
+        if(!this.userRepository.exists(id)){
+            return "redirect:/admin/users/";
+        }
+
+        User user = this.userRepository.findOne(id);
+
+        model.addAttribute("user", user);
+        model.addAttribute("view", "admin/user/delete");
+
+        return "layout";
+    }
+
+
+    //POST delete
+    @PostMapping("/delete/{id}")
+    public String deleteProcess(@PathVariable Integer id){
+
+        if(!this.userRepository.exists(id)){
+            return "redirect:/admin/users/";
+        }
+
+        User user = this.userRepository.findOne(id);
+
+
+        //Deletes all articles of the current user
+        for (Article article : user.getArticles()){
+            this.articleRepository.delete(article);
+        }
+
+        this.userRepository.delete(user);
+
+        return "redirect:/admin/users/";
     }
 
 }
